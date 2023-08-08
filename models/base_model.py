20a46a6920c416a5f6596ae16589625cc536e40a
shem <shemomondi@gmail.com>
2023-08-08 06:24:13 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,20 @@ class BaseModel:
     attributes for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """This is used to initialize the attributes
     and set up the initial state of the object
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs == NULL:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == "updated_at":
+                    value = datetime.strftime("%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """returns the informal string representation of the object
