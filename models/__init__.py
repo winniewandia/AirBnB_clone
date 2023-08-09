71be46d4dfb761a25159f8ccf7726235deb0fafe
shem <shemomondi746@gmail.com>
2023-08-09 11:44:49 +0300

    documentation

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module creates an instance of FileStorage
+"""
+
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" This module contains FileStorage class that is to serialize and deserialize
+    instances to a json file
+
+    Returns:
+        dict: The dict __object
+"""
+import json
+import os
+
+class FileStorage:
+    """This class  that serializes instances to 
+    a JSON file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """This method returns the dictionary __objects
+
+        Returns:
+            dictonary: The dictonary __object 
+        """
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        """This method sets in __objects the obj with key <obj class name>.id
+
+        Args:
+            obj: value of the dictonary
+        """
+        key = obj.__class__.__name__ + '.' + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """THis method serializes __objects to the JSON file (path: __file_path)
+        """
+        copy_dict = {}
+        for key, value in FileStorage.__objects.items():
+            copy_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(copy_dict, f)
+
+    def  reload(self):
+        """This method deserializes the JSON file to __objects (only if the JSON file
+        """
+        if os.path.exists(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r') as f:
+                FileStorage.__objects = json.load(f)
+
+                
+                   
