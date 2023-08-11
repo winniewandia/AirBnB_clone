e3857177c715ef5549c2bd917e1e2b1737698f40
winniewandia <winniewandia63@gmail.com>
2023-08-11 15:01:45 +0300

    corrected indent of console

--- console.py
+++ console.py
@@ -40,7 +40,7 @@ class HBNBCommand(cmd.Cmd):
             Boolean: True
         """
         return True
-    
+
     def do_create(self, arg):
         """Creates a new instance of BaseModel and prints the id
 
@@ -55,7 +55,7 @@ class HBNBCommand(cmd.Cmd):
             return
         class_dict = {"BaseModel": BaseModel}
 
-        instance = class_dict[arg] ()
+        instance = class_dict[arg]()
         instance.save()
         print(instance.id)
 
@@ -82,14 +82,14 @@ class HBNBCommand(cmd.Cmd):
             if object_name == args[0] and object_id == args[1].strip("''"):
                 print(value)
                 return
-            
-        print("** no instance found **") 
+
+        print("** no instance found **")
 
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id
-         Args: 
+         Args:
          arg (str): Class name and instance id
-        """  
+        """
         args = arg.split(" ")
         if not args:
             print("** class name missing **")
@@ -109,8 +109,7 @@ class HBNBCommand(cmd.Cmd):
                 del storage._FileStorage__objects[key]
                 storage.save()
                 return
-            
-            
+
         print("** no instance found **")
 
     def do_all(self, arg):
@@ -118,7 +117,7 @@ class HBNBCommand(cmd.Cmd):
 
         Args:
             arg (str): Optional class name
-     
+
         """
         args = arg.split(" ")
         if args[0] not in HBNBCommand.my_classes:
@@ -136,16 +135,20 @@ class HBNBCommand(cmd.Cmd):
         """Updates an instance based on class name and id
 
         Args:
-            arg (str): Class name, instance id, attribute name, and attribute value
-        
+            arg (str): Class name, instance id, attribute name,
+            and attribute value
+
         """
         if not arg:
             print("** class name missing **")
             return
-        my_string = ""
-        for token in arg.split(' '):
-            my_string += token
-        command_arg = shlex.split(my_string) 
+
+        # my_string = ""
+        # for token in arg.split(','):
+        #     my_string += token
+
+        command_arg = shlex.split(arg)
+
         if command_arg[0] not in HBNBCommand.my_classes:
             print("** class doesn't exist **")
         elif len(command_arg) == 1:
@@ -153,20 +156,20 @@ class HBNBCommand(cmd.Cmd):
         else:
             all_objects = storage.all()
             for key, value in all_objects.items():
-               object_name = value.__class__.__name__
-               object_id = value.id
-               if object_name == command_arg[0] and object_id == command_arg[1].strip("''"):
-                if len(command_arg) == 2:
-                    print("** attribute name missing **")
-                elif len(command_arg) == 3:
-                    print("** value missing **")
-                else:
-                    setattr(value,command_arg[2],command_arg[3])
-                    storage.save
-                return
-        print("** no instance found **")            
-  
-    
+                object_name = value.__class__.__name__
+                object_id = value.id
+                if object_name == command_arg[0] and \
+                        object_id == command_arg[1].strip('"'):
+                    if len(command_arg) == 2:
+                        print("** attribute name missing **")
+                    elif len(command_arg) == 3:
+                        print("** value missing **")
+                    else:
+                        setattr(value, command_arg[2], command_arg[3])
+                        storage.save()
+                    return
+            print("** no instance found **")
 
-    if __name__ == '__main__':
-            HBNBCommand().cmdloop()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
